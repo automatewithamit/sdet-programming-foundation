b3a25844cbe38b3b8e6eb040a0ee7d912b5ffbdd
amittripathi <automatewithamit@gmail.com>
2026-03-11 00:20:18 +0530

    Project structure

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+SDET Programming Foundation Program
+
+Student: Sonal
+Mentor: Amit
+
+This repository tracks learning progress, assignments,
+and Java programming practice for SDET preparation.
--- src/main/java/com/automatewithamit/qa/Main.java
+++
@@ -1,9 +0,0 @@
-package com.automatewithamit.qa;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("This is first commit");
-    }
-
-}
