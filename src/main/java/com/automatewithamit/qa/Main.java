b36dd13a6ab77a832ecb094302a99f3f84fe46c6
amittripathi <automatewithamit@gmail.com>
2026-03-10 22:25:42 +0530

    added print statement

--- src/main/java/com/automatewithamit/qa/Main.java
+++ src/main/java/com/automatewithamit/qa/Main.java
@@ -3,5 +3,7 @@ package com.automatewithamit.qa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is first commit");
     }
+
 }
